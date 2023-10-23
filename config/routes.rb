171f61895b37a4f99c52dbfc275b9fe1e08f5ee2
Aamir <aamirasaram2107@gmail.com>
2023-10-23 22:41:41 +0200

    Setup API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "greeting#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :greeting, only: [:index]
+    end
+  end
 end
